83fd1d9ed95ff59bb575ccabb787554001fd8bf6
Steve Tine <steve@tinefamily.com>
2025-09-10 00:19:17 -0400

    Initial working release

---
+++ ktranslate_syslog/rootfs/usr/bin/start_ktranslate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bashio
+
+#To keep the API KEY off the command line, have ktranslate pick it up from the ENV var
+export NEW_RELIC_API_KEY="$(bashio::config 'new_relic_api_key')"
+
+NEW_RELIC_ACCOUNT_ID="$(bashio::config 'new_relic_account_id')"
+
+echo Changing timezone to UTC so local container logs will have the right timestamp in NR
+export TZ='UTC'
+
+ktranslate -listen off -mapping /etc/ktranslate/config.json -geo /etc/ktranslate/GeoLite2-Country.mmdb \
+        -udrs /etc/ktranslate/udr.csv -api_devices /etc/ktranslate/devices.json -asn /etc/ktranslate/GeoLite2-ASN.mmdb \
+        -snmp /etc/ktranslate/snmp-base-hassio.yaml -log_level debug -nr_account_id="$NEW_RELIC_ACCOUNT_ID" \
+        -metrics=jchf -tee_logs=true -dns=local -service_name=home-assistant-ktranslate nr1.syslog
